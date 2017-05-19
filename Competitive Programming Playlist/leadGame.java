0c175e58d81553734870a51072aa1b73f092496b
Yash Agrawal <yagrawal900@gmail.com>
2017-05-20 04:47:33 +0530

    Edit in LeadGame.java

--- Competitive Programming Playlist/leadGame.java
+++ Competitive Programming Playlist/leadGame.java
@@ -1,11 +1,10 @@
 /* package whatever; // don't place package name! */
 
 import java.util.*;
-import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Ideone
+class Main
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
